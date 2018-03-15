300ab3b45ef6dceec3a584e699c5204506f6d7a1
msghost <zhong.yh@outlook.com>
2018-03-15 15:00:39 +0800

    数组及数组增删

--- py-test.py
+++ py-test.py
@@ -1,15 +1,14 @@
-n = 7
-i = 2
-if n>1:
-    #for i in range(2,n):
-    while i<n:
-        print("i=",i)
-        if n%i==0:
-            print(n,"不是一个质数")
-            break
-        i+=1
-    else:
-        print(n,"是一个质数")
-else:
-    print(n,"不是一个质数")
-        
+xs = ["11","22","33","44"]
+
+print(xs)
+
+xs.append("55")
+
+print(xs)
+
+xs.pop()
+
+print(xs)
+
+for x in xs:
+    print(x)
